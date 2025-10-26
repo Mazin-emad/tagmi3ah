b983b3e7265962c9e2c0e4a2cc8152c996dcb88c
HusseinMhamed <146492419+HusseinMhamed@users.noreply.github.com>
2025-10-26 21:21:18 +0300

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,94 @@
+export const products = [
+  {
+    id: "1",
+    name: "AMD Ryzen 9 5900X",
+    image:
+      "https://images.unsplash.com/photo-1591799264318-7e6ef8ddb7ea?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 100,
+    description: "AMD Ryzen 9 5900X description",
+    category: "CPU",
+    brand: "AMD",
+    stock: 5,
+  },
+  {
+    id: "2",
+    name: "Intel Core i9-12900K",
+    image:
+      "https://images.unsplash.com/photo-1591238372338-22d30c883a86?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 200,
+    description: "Intel Core i9-12900K description",
+    category: "CPU",
+    brand: "Intel",
+    stock: 77,
+  },
+  {
+    id: "3",
+    name: "Samsung 980 Pro",
+    image: "https://images.pexels.com/photos/7859350/pexels-photo-7859350.jpeg",
+    price: 300,
+    description: "Samsung 980 Pro description",
+    category: "SSD",
+    brand: "Samsung",
+    stock: 15,
+  },
+  {
+    id: "4",
+    name: "Corsair Vengeance RGB Pro",
+    image:
+      "https://images.unsplash.com/photo-1610945265064-0e34e5519bbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Ftc3VuZ3xlbnwwfHwwfHx8MA%3D%3D",
+    price: 400,
+    description: "Corsair Vengeance RGB Pro description",
+    category: "CPU",
+    brand: "AMD",
+    stock: 10,
+  },
+  {
+    id: "5",
+    name: "Corsair Vengeance RGB Pro",
+    image:
+      "https://images.unsplash.com/photo-1610945265064-0e34e5519bbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Ftc3VuZ3xlbnwwfHwwfHx8MA%3D%3D",
+    price: 500,
+    description: "Corsair Vengeance RGB Pro description",
+    category: "CPU",
+    brand: "AMD",
+    stock: 10,
+  },
+  {
+    id: "6",
+    name: "Corsair Vengeance RGB Pro",
+    image:
+      "https://images.unsplash.com/photo-1610945265064-0e34e5519bbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Ftc3VuZ3xlbnwwfHwwfHx8MA%3D%3D",
+    price: 600,
+    description: "Corsair Vengeance RGB Pro description",
+    category: "CPU",
+    brand: "AMD",
+    stock: 10,
+  },
+  {
+    id: "7",
+    name: "Corsair Vengeance RGB Pro",
+    image:
+      "https://images.unsplash.com/photo-1610945265064-0e34e5519bbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Ftc3VuZ3xlbnwwfHwwfHx8MA%3D%3D",
+    price: 700,
+    description: "Corsair Vengeance RGB Pro description",
+    category: "CPU",
+    brand: "AMD",
+    stock: 10,
+  },
+  {
+    id: "8",
+    name: "Corsair Vengeance RGB Pro",
+    image:
+      "https://images.unsplash.com/photo-1610945265064-0e34e5519bbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Ftc3VuZ3xlbnwwfHwwfHx8MA%3D%3D",
+    price: 800,
+    description: "Corsair Vengeance RGB Pro description",
+    category: "CPU",
+    brand: "AMD",
+    stock: 10,
+  },
+];
+
+
 export const categories = [
   "CPU",
   "GPU",
