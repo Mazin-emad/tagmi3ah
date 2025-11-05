566f46fc061eefc897ef515794005874fe148c05
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-11-05 17:10:48 +0200

    ggg

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
+import { MyItemsContextProvider } from "./Context/myItems.tsx";
 
 // Create a QueryClient with sensible defaults
 const queryClient = new QueryClient({
@@ -21,10 +22,12 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <MyItemsContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </MyItemsContextProvider>
   </StrictMode>
 );
