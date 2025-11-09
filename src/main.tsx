a8daf71fcef834173383c8cb6bc9f7b65e1bb5ca
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-11-09 20:14:58 +0200

    gf3

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
 import { MyItemsContextProvider } from "./Context/MyItemsProvider";
@@ -21,6 +22,8 @@ const queryClient = new QueryClient({
   },
 });
 
+const isDev = import.meta.env.VITE_DEV === "true";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MyItemsContextProvider>
@@ -29,6 +32,7 @@ createRoot(document.getElementById("root")!).render(
           <BrowserRouter>
             <App />
           </BrowserRouter>
+          {isDev && <ReactQueryDevtools initialIsOpen={false} />}
         </QueryClientProvider>
       </CartProvider>
     </MyItemsContextProvider>
